8b12e12cd543d7ed772928216c09e902f4be153b
Mead Solomon <mead93@gmail.com>
2024-02-25 20:09:17 -0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  publicDir: "static/",
-  base: "./",
-  server: { port: 2024 },
-  plugins: [glsl()],
-  build: {
-    outDir: "dist",
-  },
-});
+  export default defineConfig({
+    root: "src/",
+    publicDir: "static",
+    base: "./",
+    server: { port: 2024 },
+    plugins: [glsl()],
+    build: {
+      outDir: "./dist",
+    },
+  });
